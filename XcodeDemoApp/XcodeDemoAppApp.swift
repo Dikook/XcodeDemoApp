ba4ca9766de76e8db0fe8f0c58352d72d301fda1
Dikook <vimvom931@gmail.com>
2024-06-12 12:17:30 +0500

    Initial Commit

---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by Diar Orynbek on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
